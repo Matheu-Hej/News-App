8da1ac4fdeaa11135c2d5e00998f5c00be7035b7
Matheu-Hej <matej.hejzlar@protonmail.com>
2026-01-26 22:06:16 +0100

    Added watchlist middlewares to authenticate the JWT Token and separate the userId | 2:01:31

---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,37 @@
+import jwt from 'jsonwebtoken';
+import { prisma } from "../config/db.js";
+
+
+// Read the token from the request
+// Check if the token is valid
+export const authMiddleware = async (req, res, next) => {
+    console.log("Auth middleware reached");
+    let token;
+
+    if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+        token = req.headers.authorization.split(" ")[1]; // Split into ["Bearer", "TOKEN"]
+    } else if (req.cookies.jwt) {
+        token = req.cookies.jwt;
+    }
+
+    if (!token) {
+        return res.status(401).json({error: "Invalid or no token provided"});
+    }
+
+    try {
+        // Verify the token is valid and extract the userId
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await prisma.user.findUnique({
+            where: {id: decoded.id},
+        });
+
+        if (!user) {
+            return res.status(401).json({ error: "User no longer exists"});
+        }
+
+        req.user = user;
+        next();
+    } catch (err) {
+        return res.status(401).json({ error: "Authentication Failed" });
+    }
+};
